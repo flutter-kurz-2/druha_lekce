ab8e590485a8861b1b5db7baa1247d833ec92991
Vojtěch Pšenák <me@vojtapsenak.cz>
2021-11-03 14:25:45 +0100

    add second screen

---
+++ lib/screens/second_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: const Text("Titulek"),
+      ),
+      body: Container(
+      ),
+    );
+  }
+}

07d4eb9b41d3c6e4a5f93b72984a10133dd68ef7
Vojtěch Pšenák <me@vojtapsenak.cz>
2021-11-10 14:47:48 +0100

    camera screen

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({Key? key}) : super(key: key);
@@ -8,19 +9,49 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+  CameraController? controller;
+  List<CameraDescription> cameras = [];
+
+
   @override
   void initState() {
     super.initState();
+    readyCameras();
+
+  }
+
+  Future getCameras() async {
+    cameras = await availableCameras();
+  }
+
+  Future readyCameras() async {
+    await getCameras();
+    controller = CameraController(cameras[0], ResolutionPreset.max);
+    controller!.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    if (controller == null || !controller!.value.isInitialized) {
+      return Container();
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text("Second Screen"),
       ),
       body: Container(
-        
+        child: CameraPreview(controller!),
       ),
     );
   }
